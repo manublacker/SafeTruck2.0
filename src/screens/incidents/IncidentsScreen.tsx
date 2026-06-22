f212e589b2c18e80c49bcbedc0a675d40ff34d6c
lucaMesi <lmesiti@itba.edu.ar>
2026-06-22 20:53:25 -0300

    limpieza

--- src/screens/incidents/IncidentsScreen.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-export default function IncidentsScreen() {
-  return <View><Text>Incidents</Text></View>
-}
--- src/screens/profile/VehiclesScreen.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-export default function VehiclesScreen() {
-  return <View><Text>Vehicles</Text></View>
-}
